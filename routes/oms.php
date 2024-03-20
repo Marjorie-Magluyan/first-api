ab133c02cc47595e5cb66189f98c45dba45891b1
Marjorie <marjorieanito@student.laverdad.edu.ph>
2024-03-20 23:31:13 +0800

    route

--- routes/oms.php
+++ routes/oms.php
@@ -19,7 +19,34 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/my-api', function () {
-    return 'Hello World!';
+// Route::get('/my-api', function () {
+//     return 'Hello World!';
+// });
+
+Route::get('/api-get', function () {
+    return "Route; GET method";
 });
 
+Route::post('/api-post', function () {
+    return "Route; POST method";
+});
+
+Route::put('/api-put', function () {
+    return "Route; PUT method";
+});
+
+Route::put('/api-put', function () {
+    return "Route; PUT method";
+});
+
+Route::patch('/api-patch', function () {
+    return "Route; PATCH method";
+});
+
+Route::delete('/api-delete', function () {
+    return "Route; DELETE method";
+});
+
+Route::options('/api-options', function () {
+    return "Route; OPTIONS method";
+});
